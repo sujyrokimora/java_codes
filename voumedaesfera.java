9bcedde739b99f0bbf1bdb8079b0f9034cabb5a1
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-10-13 15:26:10 +0100

    Create voumedaesfera.java

---
+++ voumedaesfera.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.*;
+import java.lang.Math.*;
+
+public class Main {
+    public static void main(String args[]) {
+        double r = 2.0;
+        double v = 0;
+
+        System.out.println("O volume da esfera é " + 0.75*Math.PI* Math.pow(r,3));
+    }
+}
