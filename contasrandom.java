f573f634a4f266e6dc8d38818d1ba79b83ef42a0
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-10-13 16:49:03 +0100

    Update contasrandom.java

--- contasrandom.java
+++ contasrandom.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.lang.Math.*;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println(2.0 + (3.0 / ((Math.pow(4,43.58) - 5.0) / (Math.sin(Math.PI / 2.0)))));
 
 
