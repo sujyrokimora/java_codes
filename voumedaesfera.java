f42cc1f6b83959dc536e6428d244702a01d911e8
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-10-13 16:49:35 +0100

    Update voumedaesfera.java

--- voumedaesfera.java
+++ voumedaesfera.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.lang.Math.*;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         double r = 2.0;
         double v = 0;
 
