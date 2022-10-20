8021f39d994e3eda6649f908a3946295ba3674c2
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-10-20 15:45:26 +0100

    Update equacao.java

--- equacao.java
+++ equacao.java
@@ -16,6 +16,39 @@ public class Main {
         {
             double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
             double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+            package org.example;
+import java.util.*;
+import java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter the value of b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter the value of c: ");
+        double c = input.nextDouble();
+        double d= b * b - 4.0 * a * c;
+        if (d> 0.0)
+        {
+            double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+            //double r1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c) / (2.0 * a));
+            //double r2 = (-b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c) / (2.0 * a)));
+            double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+            System.out.println("The roots are " + r1 + " and " + r2);
+        }
+        else if (d == 0.0)
+        {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        }
+        else
+        {
+            System.out.println("Roots are not real.");
+        }
+    }
+}
             System.out.println("The roots are " + r1 + " and " + r2);
         }
         else if (d == 0.0)
