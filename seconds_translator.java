b5028ebb4180ae039830caac0aa7f220935cf92e
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-11-03 15:31:32 +0000

    Update seconds_translator.java

--- seconds_translator.java
+++ seconds_translator.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.util.*;
 import java.math.*;
 import java.time.*;
@@ -7,21 +8,22 @@ public class Main {
     public static void main(String[] args) {
         time();
     }
+
     public static void time() {
         System.out.println("\n Insert a time using : to separate it (format HH:mm:ss)");
-        String time= scanner.next();
-        String[] fulltime= time.split(":");
-        if ((fulltime.length<=1)||(fulltime.length>3)) {
+        String time = scanner.next();
+        String[] fulltime = time.split(":");
+        if ((fulltime.length <= 1) || (fulltime.length > 3)) {
             System.out.println("\n Invalid time please, try again!");
             time();
-        }
-        else{
+        } else {
             // position 0 is equivalent to hour
             // position 1 is equivalent to minute
             // position 2 is equivalent to second
-            int fullseconds=(Integer.parseInt(fulltime[0])*3600)+ (Integer.parseInt(fulltime[1])*60)+ Integer.parseInt(fulltime[2]);
-            System.out.println("\n This is equal to "+fullseconds+ "seconds");
+            int fullseconds = (Integer.parseInt(fulltime[0]) * 3600) + (Integer.parseInt(fulltime[1]) * 60) + Integer.parseInt(fulltime[2]);
+            System.out.println("\n This is equal to " + fullseconds + "seconds");
         }
     }
+
     public static Scanner scanner = new Scanner(System.in).useLocale(Locale.ROOT);
 }
