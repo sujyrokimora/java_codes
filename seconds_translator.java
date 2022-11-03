c005f9fbd5b767ec849172f24779e98779891f08
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-11-03 16:09:28 +0000

    Update seconds_translator.java

--- seconds_translator.java
+++ seconds_translator.java
@@ -20,8 +20,7 @@ public class Main {
             // position 0 is equivalent to hour
             // position 1 is equivalent to minute
             // position 2 is equivalent to second
-            int fullseconds = (Integer.parseInt(fulltime[0]) * 3600) + (Integer.parseInt(fulltime[1]) * 60) + Integer.parseInt(fulltime[2]);
-            System.out.println("\n This is equal to " + fullseconds + " seconds");
+            System.out.println("\n This is equal to " + (Integer.parseInt(fulltime[0]) * 3600) + (Integer.parseInt(fulltime[1]) * 60) + Integer.parseInt(fulltime[2]) + " seconds");
         }
     }
 
