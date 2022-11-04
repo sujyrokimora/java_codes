7fd06494034374e3277e8d46aab7a70b98bbbe55
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-11-04 12:12:36 +0000

    Update array_2.java

--- array_2.java
+++ array_2.java
@@ -1,18 +1,16 @@
 package org.example;
 
 import java.util.*;
-import java.math.*;
-import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array= {40,36,30,28,23,18,10,7,3};
+        int[] array = {40, 36, 30, 28, 23, 18, 10, 7, 3};
 
-        for(int i=0;i<array.length;i++)
-        {
-            System.out.println((i+1)+"º: "+ array[i]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i + 1) + "º: " + array[i]);
         }
 
     }
+
     public static Scanner scanner = new Scanner(System.in).useLocale(Locale.ROOT);
 }
