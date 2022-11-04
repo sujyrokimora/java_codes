bc22b1ae55bb0c158d1da996fb083f35dc8436de
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-11-04 12:08:54 +0000

    Create array.java

---
+++ array.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.*;
+import java.math.*;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array= {40,36,30,28,23,18,10,7,3};
+        System.out.print("Insira a posição que quer ver o numero de pontos: \t");
+        System.out.println(array[scanner.nextInt()-1]);
+    }
+    public static Scanner scanner = new Scanner(System.in).useLocale(Locale.ROOT);
+}
