7e52e024a1a749f825032211362666b95a1553d3
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-11-04 12:30:07 +0000

    Update array_3.java

--- array_3.java
+++ array_3.java
@@ -4,13 +4,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] array = {{"40","Beja"},{"36","Cuba"}, {"30","Pedrogao"}, {"28","Ferreira"}, {"23","Aljustrel"}, {"18","Serpa"}, {"10","Serpa"}, {"7","Vidigeuria"}, {"3","Evora"}};
+        String[][] array = {{"40", "Beja"}, {"36", "Cuba"}, {"30", "Pedrogao"}, {"28", "Ferreira"}, {"23", "Aljustrel"}, {"18", "Serpa"}, {"10", "Serpa"}, {"7", "Vidigeuria"}, {"3", "Evora"}};
 
         for (int i = 0; i < array.length; i++) {
-            System.out.println((i + 1) + "º: " + array[i][0]+" - "+array[i][1]);
+            System.out.println((i + 1) + "º: " + array[i][0] + " - " + array[i][1]);
         }
 
     }
-
-    public static Scanner scanner = new Scanner(System.in).useLocale(Locale.ROOT);
 }
